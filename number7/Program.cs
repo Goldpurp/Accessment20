ebf6233c994d26cc7073c8034bf9b99642f7898d
GoldPurpp <lilswatch112@gmail.com>
2021-05-24 20:12:55 +0800

    new ifo added

---
+++ number7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace number7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable1 = 45;
+
+            int variable2 = 25;
+
+            int sum = variable1 + variable2;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
